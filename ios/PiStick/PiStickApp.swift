875559a578cb23e9235ae79fa28b44daf85b7590
tnichol00 <tnichol00@gmail.com>
2026-08-14 14:56:50 -0400

    Keep unit tests independent of app web startup

--- ios/PiStick/PiStickApp.swift
+++ ios/PiStick/PiStickApp.swift
@@ -4,6 +4,21 @@ import SwiftUI
 struct PiStickApp: App {
     var body: some Scene {
         WindowGroup {
+            AppRootView()
+        }
+    }
+}
+
+private struct AppRootView: View {
+    private var isRunningUnitTests: Bool {
+        ProcessInfo.processInfo.environment["XCTestConfigurationFilePath"] != nil
+    }
+
+    @ViewBuilder
+    var body: some View {
+        if isRunningUnitTests {
+            Color.black.ignoresSafeArea()
+        } else {
             ContentView()
                 .preferredColorScheme(.dark)
         }
